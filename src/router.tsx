ec393c5bece44bb8d8f619411ef3b4eab3d14b7d
Tom <99224451+ato-m-a@users.noreply.github.com>
2023-11-17 10:33:31 +0900

    hashRouter 사용

--- src/router.tsx
+++ src/router.tsx
@@ -1,17 +1,17 @@
 import Main from '@/pages';
 import EasySystem from '@/pages/portfolio/easy-system';
 import Eventworld from '@/pages/portfolio/eventworld';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 const Router = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/portfolio/사전청약-페이지-간편-구축-시스템" element={<EasySystem />} />
-        <Route path="/portfolio/이벤트월드" element={<Eventworld />} />
+        <Route path="/사전청약-페이지-간편-구축-시스템" element={<EasySystem />} />
+        <Route path="/이벤트월드" element={<Eventworld />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
